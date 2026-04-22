b36fb937c5972e5c2f0f1325ec0edcbbdf85bfa2
Dadinaut <dadinaut@gmail.com>
2026-04-22 23:22:20 +0200

    types errors cleaned

--- backend/src/utils/spreadPostsResponses.ts
+++ backend/src/utils/spreadPostsResponses.ts
@@ -1,73 +1,73 @@
-import { prisma } from "../lib/prisma";
+// import { prisma } from "../lib/prisma";
 
-interface Response {
-  id: number;
-  userId: number;
-  postId: number;
-  parentId: null | number;
-  createdAt?: Date | null;
-  updatedAt?: Date | null;
-  replies?: Response[];
-}
+// interface Response {
+//   id: number;
+//   userId: number;
+//   postId: number;
+//   parentId: null | number;
+//   createdAt?: Date | null;
+//   updatedAt?: Date | null;
+//   replies?: Response[];
+// }
 
-export async function getAllOnePostResponses(postId: number) {
-  async function getReplies(responses: Response[]) {
-    return await Promise.all(
-      responses.map(async (response): Promise<Response> => {
-        const replies = await prisma.response.findMany({
-          where: {
-            parentId: response.id,
-          },
-          include: {
-            user: {
-              select: {
-                id: true,
-                firstname: true,
-                lastname: true,
-                avatarUrl: true,
-              },
-            },
-            replies: true,
-          },
-        });
+// export async function getAllOnePostResponses(postId: number) {
+//   async function getReplies(responses: Response[]) {
+//     return await Promise.all(
+//       responses.map(async (response): Promise<Response> => {
+//         const replies = await prisma.response.findMany({
+//           where: {
+//             parentId: response.id,
+//           },
+//           include: {
+//             user: {
+//               select: {
+//                 id: true,
+//                 firstname: true,
+//                 lastname: true,
+//                 avatarUrl: true,
+//               },
+//             },
+//             replies: true,
+//           },
+//         });
 
-        if (replies.length > 0) {
-          return {
-            ...response,
-            replies: await getReplies(replies),
-          };
-        }
+//         if (replies.length > 0) {
+//           return {
+//             ...response,
+//             replies: await getReplies(replies),
+//           };
+//         }
 
-        return {
-          ...response,
-          replies: [],
-        };
-      }),
-    );
-  }
+//         return {
+//           ...response,
+//           replies: [],
+//         };
+//       }),
+//     );
+//   }
 
-  try {
-    // Get parent responses
-    const responses = await prisma.response.findMany({
-      where: {
-        postId,
-        parentId: null,
-      },
-      include: {
-        user: {
-          select: {
-            id: true,
-            firstname: true,
-            lastname: true,
-            avatarUrl: true,
-          },
-        },
-        replies: true,
-      },
-    });
+//   try {
+//     // Get parent responses
+//     const responses = await prisma.response.findMany({
+//       where: {
+//         postId,
+//         parentId: null,
+//       },
+//       include: {
+//         user: {
+//           select: {
+//             id: true,
+//             firstname: true,
+//             lastname: true,
+//             avatarUrl: true,
+//           },
+//         },
+//         replies: true,
+//       },
+//     });
 
-    return await getReplies(responses);
-  } catch (err) {
-    console.log(err);
-  }
-}
+//     return await getReplies(responses);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
