adad2dc33a333e6553dc91f9ab867726a84283ca
Dadinaut <dadinaut@gmail.com>
2026-04-23 07:01:51 +0200

    cors origin updated

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,13 +7,18 @@ import { mealsRouter } from "./routes/mealsRouter.js";
 import { restaurantsRouter } from "./routes/restaurantsRouter.js";
 import { mealCategoriesRouter } from "./routes/mealCategoriesRouter.js";
 import { cityRouter } from "./routes/cityRouter.js";
+import { verifyToken } from "./controllers/verifyToken.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://restaurant-marketplace.onrender.com",
+  }),
+);
 
 app.use(express.json());
 app.use("/api/auth", authRouter);
@@ -22,7 +27,7 @@ app.use("/api/meals", mealsRouter);
 app.use("/api/meal-categories", mealCategoriesRouter);
 app.use("/api/city", cityRouter);
 // app.use("/api/category", categoryRouter);
-// app.post("/api/verify-token", verifyToken);
+app.post("/api/verify-token", verifyToken);
 
 app.use((req, res) => {
   res.status(404).json({
